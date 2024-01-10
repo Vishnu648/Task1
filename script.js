99168cf6ea2a4f5d7f642b9940013e581eeedd04
Vishnu V S <vishnusenthil222@gmail.com>
2024-01-10 09:50:28 +0530

    register and login complete

--- script.js
+++ script.js
@@ -8,7 +8,18 @@ function register() {
     password.length > 3 &&
     confirmPassword.length > 3
   ) {
-    window.location.href = "/login.html";
+    if (password == confirmPassword) {
+      let credentials = {
+        username: username,
+        password: password,
+      };
+      let stringCredentials = JSON.stringify(credentials);
+      localStorage.setItem("credentials", stringCredentials);
+      document.getElementById("username").value = "";
+      document.getElementById("password").value = "";
+      document.getElementById("confirmPassword").value = "";
+      window.location.href = "/login.html";
+    }
   }
 }
 
@@ -16,4 +27,22 @@ function register() {
 function login() {
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
+
+  let credentials = localStorage.credentials;
+  console.log(typeof credentials, credentials);
+
+  const parsedCredentials = JSON.parse(credentials);
+  console.log(typeof parsedCredentials, parsedCredentials);
+
+  if (
+    username == parsedCredentials.username &&
+    password == parsedCredentials.password
+  ) {
+    window.location.href = "/home.html";
+  }
+}
+
+/* home */
+function home() {
+  console.log('home loaded');
 }
