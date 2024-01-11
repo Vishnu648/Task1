722ac7b3e478c2231507d2f1908a6851d477118a
Vishnu V S <vishnusenthil222@gmail.com>
2024-01-11 14:37:14 +0530

    validation error

--- script.js
+++ script.js
@@ -15,15 +15,43 @@ function register() {
       };
       let stringCredentials = JSON.stringify(credentials);
       localStorage.setItem("credentials", stringCredentials);
-      document.getElementById("confirmPassword").value = "";
       window.location.href = "/login.html";
     }
-  } else if (
-    // username.length <= 1 ||
-    password.length <= 3 ||
-    confirmPassword.length <= 3
-  ) {
-    alert('password should be atleast 4 characters');
+  }
+  if (username.length < 3) {
+    let require = "name should contain atleast 3 characters";
+    document.getElementById("nameRequired").innerText = require;
+    document.getElementById("nameRequired").style.color = "red";
+  }
+  if (username.length == 0) {
+    let require = "username is required";
+    document.getElementById("nameRequired").innerText = require;
+    document.getElementById("nameRequired").style.color = "red";
+  }
+  if (password.length < 4) {
+    let require = "password is required";
+    document.getElementById("passwordRequired").innerText = require;
+    document.getElementById("passwordRequired").style.color = "red";
+  }
+  if (confirmPassword.length < 4 && confirmPassword.length > 0) {
+    let require = "confirm password is required";
+    document.getElementById("confirmPassRequired").innerText = require;
+    document.getElementById("confirmPassRequired").style.color = "red";
+  }
+  if (confirmPassword.length == 0) {
+    let require = "confirm password is required";
+    document.getElementById("confirmPassRequired").innerText = require;
+    document.getElementById("confirmPassRequired").style.color = "red";
+  }
+  if (password.length > 0 && password.length < 4) {
+    let require = "password should contain min 4 characters";
+    document.getElementById("passwordRequired").innerText = require;
+    document.getElementById("passwordRequired").style.color = "red";
+  }
+  if (password != confirmPassword) {
+    let require = "password do not match";
+    document.getElementById("confirmPassRequired").innerText = require;
+    document.getElementById("confirmPassRequired").style.color = "red";
   }
 }
 
@@ -41,11 +69,29 @@ function login() {
     password == parsedCredentials.password
   ) {
     window.location.href = "/home.html";
-  } else if (
-    username != parsedCredentials.username ||
-    password != parsedCredentials.password
-  ) {
-    alert("UserName and Password don't match");
+  }
+  if (username.length == 0) {
+    let require = "username is required";
+    document.getElementById("loginNameRequired").innerText = require;
+    document.getElementById("loginNameRequired").style.color = "red";
+  }
+  if (password.length == 0) {
+    let require = "password is required";
+    document.getElementById("loginPassRequired").innerText = require;
+    document.getElementById("loginPassRequired").style.color = "red";
+  }
+  if (username.length != 0 && password.length != 0) {
+    document.getElementById("loginNameRequired").innerText = "";
+    document.getElementById("loginPassRequired").innerText = "";
+
+    if (
+      username != parsedCredentials.username ||
+      password != parsedCredentials.password
+    ) {
+      let noMatch = "Username and Password didn't match";
+      document.getElementById("noMatch").innerText = noMatch;
+      document.getElementById("noMatch").style.color = "red";
+    }
   }
 }
 
